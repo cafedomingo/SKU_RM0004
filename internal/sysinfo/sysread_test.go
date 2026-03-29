e0d1e954a7e3e5867ebf7f7cd47f833990d127cf
Patrick Sunday <33767+cafedomingo@users.noreply.github.com>
2026-03-28 23:18:15 -0700

    Migrate pi_test.go tests to sysread_test.go

--- internal/sysinfo/sysread_test.go
+++ internal/sysinfo/sysread_test.go
@@ -1,6 +1,33 @@
 package sysinfo
 
-import "testing"
+import (
+	"log/slog"
+	"testing"
+)
+
+func TestCPUFreqRead(t *testing.T) {
+	r := NewSystemReader(slog.Default())
+	f := r.CPUFreq()
+	if f.Cur == 0 && f.Min == 0 && f.Max == 0 {
+		t.Log("CPUFreq() returned all zeros (expected on non-Pi)")
+	}
+}
+
+func TestDietPiDetection(t *testing.T) {
+	r := NewSystemReader(slog.Default())
+	s := r.DietPiStatus()
+	if s < DietPiNotInstalled || s > DietPiUpdateAvail {
+		t.Errorf("DietPiStatus() = %d, not a valid DietPiStatus", s)
+	}
+}
+
+func TestAPTUpdateCount(t *testing.T) {
+	r := NewSystemReader(slog.Default())
+	n := r.APTUpdateCount()
+	if n < -1 {
+		t.Errorf("APTUpdateCount() = %d, want >= -1", n)
+	}
+}
 
 func TestIsWholeDisk(t *testing.T) {
 	tests := []struct {
